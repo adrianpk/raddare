7ec628b6f75d0b64d42b9e17529ffc67ef724b27
Adrian PK <adrianpk@adrianpk.com>
2019-09-07 18:22:41 +0200

    OSRM related struct moved to its own package.

--- pkg/route/model.go
+++ pkg/route/model.go
@@ -27,35 +27,3 @@ type (
 		Distance    float64 `json:"distance"`
 	}
 )
-
-// OSRMRequest maps request data sent OSRM.
-// Ref.: http://project-osrm.org/docs/v5.5.1/api/#nearest-service
-type (
-	OSRMRequest struct {
-		OSRMRoutes    []OSRMRoutes    `json:"routes"`
-		OSRMWaypoints []OSRMWaypoints `json:"waypoints"`
-		OSRMCode      string          `json:"code"`
-	}
-
-	OSRMLegs struct {
-		Summary  string        `json:"summary"`
-		Weight   int           `json:"weight"`
-		Duration float64       `json:"duration"`
-		Steps    []interface{} `json:"steps"`
-		Distance float64       `json:"distance"`
-	}
-
-	OSRMRoutes struct {
-		Legs       []OSRMLegs `json:"legs"`
-		WeightName string     `json:"weight_name"`
-		Weight     int        `json:"weight"`
-		Duration   float64    `json:"duration"`
-		Distance   float64    `json:"distance"`
-	}
-
-	OSRMWaypoints struct {
-		Hint     string    `json:"hint"`
-		Name     string    `json:"name"`
-		Location []float64 `json:"location"`
-	}
-)
