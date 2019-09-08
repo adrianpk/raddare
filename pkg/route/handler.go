d2c799201ecab4263bf1e9938af19ec51d8f571f
Adrian PK <adrianpk@adrianpk.com>
2019-09-08 14:17:06 +0200

    Handler getRoutesHandler updated.

--- pkg/route/handler.go
+++ pkg/route/handler.go
@@ -1,18 +1,29 @@
 package route
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
 func (m *Manager) getRoutesHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("getRoutesHandler"))
+	ctx := r.Context()
+
+	wps, ok := ctx.Value(waypointsCtxKey).(*Waypoints)
+	if !ok {
+		err := errors.New("incomplete coords data")
+		m.errorResponse(w, r, err)
+	}
+
+	out := fmt.Sprintf("getRoutesHandler:\n\n%+v", wps)
+
+	w.Write([]byte(out))
 }
 
-func (m *Manager) errorResponse(w http.ResponseWriter, r *http.Request, message string, err error) {
+func (m *Manager) errorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusBadRequest)
-	str := `{"data":"","error":"Invalid request"}`
+	str := fmt.Sprintf(`{"data":"","error":"%s"}`, err.Error())
 	fmt.Fprint(w, str)
-	m.Log().Error(err, message)
+	m.Log().Error(err, err.Error())
 }
