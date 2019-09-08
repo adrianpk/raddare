1a7e028098e5362dda5e30d628e3100f409e7796
Adrian PK <adrianpk@adrianpk.com>
2019-09-08 22:39:27 +0200

    Best route selection algorithm.

---
+++ pkg/route/calc.go
@@ -0,0 +1,23 @@
+package route
+
+import (
+	"sort"
+
+	"github.com/raddare/internal/osrm"
+)
+
+func (m *Manager) bestRoute(responses []*osrm.Response) *osrm.Response {
+	sort.Slice(responses, func(i, j int) bool {
+
+		// If durations are the same...
+		if responses[i].Routes[0].Duration == responses[j].Routes[0].Duration {
+			// Use distance choosing the shortes one
+			return responses[i].Routes[0].Distance < responses[j].Routes[0].Distance
+		}
+
+		// Otherwise use duration.
+		return responses[i].Routes[0].Duration < responses[j].Routes[0].Duration
+	})
+
+	return responses[0]
+}
