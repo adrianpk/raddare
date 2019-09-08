41e52c8f665a2f074a420207e25c5a6f059fec3b
Adrian K <adrian.git@kuguar.dev>
2019-09-08 13:57:01 +0200

    Routes cURL shell script updated.

--- scripts/rest/routes.sh
+++ scripts/rest/routes.sh
@@ -9,11 +9,11 @@ PATH="routes"
 SRC_LAT="13.388860"
 SRC_LNG="52.517037"
 # DST 1
-DST_LAT_1="13.397634"
-DST_LNG_1="52.529407"
+DST_LAT_1="23.397634"
+DST_LNG_1="62.529407"
 # DST 2
-DST_LAT_2="13.428555"
-DST_LNG_2="52.523219"
+DST_LAT_2="33.428555"
+DST_LNG_2="72.523219"
 
 # Pre
 # Curl and jq installed using nix not found if path not appropriately set
@@ -29,5 +29,5 @@ post () {
 
 
 # Sample GET request: http://your-service/routes?src=13.388860,52.517037&dst=13.397634,52.529407&dst=13.428555,52.523219
-post "http://$HOST:$PORT/$PATH?src=$SRC_LAT,$SRC_LNGi&$DST_LAT_1,$DST_LNG_1&$DST_LAT_2,$DST_LNG_2"
+post "http://$HOST:$PORT/$PATH?src=$SRC_LAT,$SRC_LNG&dst=$DST_LAT_1,$DST_LNG_1&dst=$DST_LAT_2,$DST_LNG_2"
 
