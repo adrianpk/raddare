0508ec6102322d8fbfe177d59b0aee4f21ef85bb
Adrian K <adrian.git@kuguar.dev>
2019-09-08 19:06:21 +0200

    Minor code edit.

--- internal/osrm/http.go
+++ internal/osrm/http.go
@@ -23,63 +23,6 @@ type (
 	}
 )
 
-// OSRMRequest maps request data sent OSRM.
-// Ref.: http://project-osrm.org/docs/v5.5.1/api/#nearest-service
-type (
-	// Request is a container for
-	// generic OSRM query parameters.
-	Request struct {
-		Service string
-		Version string
-		Profile string
-		Points  []Point
-		Options Options
-	}
-
-	// Options is an alias for a map of type [string]string
-	// used to store options for OSRM requests.
-	Options map[string]string
-
-	// Point is an alias for a two float64 elements array.
-	// used to store geo location coordinates.
-	Point [2]float64
-)
-
-type (
-	// Response for OSRM request.
-	Response struct {
-		Routes    []Routes    `json:"routes"`
-		Waypoints []Waypoints `json:"waypoints"`
-		Code      string      `json:"code"`
-	}
-
-	// Legs values.
-	Legs struct {
-		Summary  string        `json:"summary"`
-		Weight   float64       `json:"weight"`
-		Duration float64       `json:"duration"`
-		Steps    []interface{} `json:"steps"`
-		Distance float64       `json:"distance"`
-	}
-
-	// Routes values
-	Routes struct {
-		Legs       []Legs  `json:"legs"`
-		WeightName string  `json:"weight_name"`
-		Weight     float64 `json:"weight"`
-		Duration   int     `json:"duration"`
-		Distance   float64 `json:"distance"`
-	}
-
-	// Waypoints values
-	Waypoints struct {
-		Hint     string    `json:"hint"`
-		Distance float64   `json:"distance"`
-		Name     string    `json:"name"`
-		Location []float64 `json:"location"`
-	}
-)
-
 // newClient creates a custom HTTP client.
 func newClient(url string) client {
 	var c = &http.Client{
@@ -196,9 +139,10 @@ func (c client) MakeRequest(ctx context.Context, req *Request, res interface{}) 
 	}
 
 	// Unmarshall response.
-	fmt.Printf("\n%s\n", string(bytes))
+	fmt.Printf("\n%s\n", string(bytes)) // TODO: Remove this line
 	err = json.Unmarshal(bytes, res)
 	if err != nil {
+		fmt.Printf("\n%s\n", err.Error()) // TODO: Remove this line
 		return fmt.Errorf("response body cannot be unmarshalled")
 	}
 
